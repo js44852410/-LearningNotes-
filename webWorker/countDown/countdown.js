9a1e79e8713c934fb38aa86f6faa2d3b657eee28
liulipeng <js44852410@163.com>
2016-12-19 15:43:55 +0800

    倒计时

--- webWorker/countDown/countdown.js
+++ webWorker/countDown/countdown.js
@@ -2,18 +2,19 @@
  * Created by llp on 2016/12/19.
  *
  * params Object
- *  element:元素对象或元素列表
  *  time:倒计时时间，String/Number/Date对象
- *  type:
+ *  format:返回时间格式，dd:hh:mm:ss/hh:mm:ss, 默认dd:hh:mm:ss
+ *  type:天、时、分、秒为0时处理方案 Number
+ *    0:不显示0项(默认)
+ *    1:显示0项
  * ES6
  */
 
 class CountDown {
   constructor(params){
-    this.element = params.element;
     this.time = params.time;
+    this.format = params.format && params.format.toLowerCase() == "hh:mm:ss" ? "hh:mm:ss" : "dd:hh:mm:ss";
     this.type = params.type;
-    this.callback = params.callback;
 
     this._init();
   }
@@ -36,15 +37,62 @@ class CountDown {
     }
   }
 
+  toDouble(num){
+    return num < 10 ? `0${num}` : num;
+  }
+
   initDom(){
     if(this.time < 0){
       postMessage(this.time);
     } else {
-      let second = this.time%60,
-        minute = Math.floor(this.time/60)%60,
-        hour = Math.floor(this.time/3600);
+      let innerHTML = "",
+          second = this.time%60,
+          minute = Math.floor(this.time/60)%60;
 
-      postMessage(`<span>${hour}</span>时<span>${minute}</span>分<span>${second}</span>秒`);
+      if(this.format == 'hh:mm:ss'){
+        let hour = Math.floor(this.time/3600);
+
+        if(this.type){
+          innerHTML = `<span>${this.toDouble(hour)}</span>时<span>${this.toDouble(minute)}</span>分<span>${this.toDouble(second)}</span>秒`;
+        } else {
+          if(hour){
+            innerHTML += `<span>${this.toDouble(hour)}</span>时`;
+          }
+
+          if(minute){
+            innerHTML += `<span>${this.toDouble(minute)}</span>分`;
+          }
+
+          if(second){
+            innerHTML += `<span>${this.toDouble(second)}</span>秒`;
+          }
+        }
+      } else {
+        let hour = Math.floor(this.time/3600)%24,
+            day = Math.floor(this.time/(3600*24));
+
+        if(this.type){
+          innerHTML = `<span>${this.toDouble(day)}</span>天<span>${this.toDouble(hour)}</span>时<span>${this.toDouble(minute)}</span>分<span>${this.toDouble(second)}</span>秒`;
+        } else {
+          if(day){
+            innerHTML += `<span>${this.toDouble(day)}</span>天`;
+          }
+
+          if(hour){
+            innerHTML += `<span>${this.toDouble(hour)}</span>时`;
+          }
+
+          if(minute){
+            innerHTML += `<span>${this.toDouble(minute)}</span>分`;
+          }
+
+          if(second){
+            innerHTML += `<span>${this.toDouble(second)}</span>秒`;
+          }
+        }
+      }
+
+      postMessage(innerHTML);
     }
   }
 
